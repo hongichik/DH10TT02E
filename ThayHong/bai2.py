7362652f67a4e4c00cfe5ea721d1e7580c0e71a3
hongchik <hongichik321@gmail.com>
2026-01-30 15:01:05 +0700

    bai 2

---
+++ ThayHong/bai2.py
@@ -0,0 +1,15 @@
+import random
+
+while True:
+    sodoan = int(input("Nhập số bạn đoán (1-10): "))
+    so = random.randint(1, 10)
+    if so == sodoan:
+        print("Chúc mừng bạn đã đoán đúng!")
+    elif sodoan == 99:
+        print("Bạn đã chọn thoát trò chơi.")
+        break
+    elif so > sodoan:
+        print("Số bạn đoán nhỏ hơn số máy chọn.")
+    else:
+        print("Số bạn đoán lớn hơn số máy chọn.")
+    
