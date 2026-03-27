b1c915a2448eeefa3016c659e8c3a6b63a7191de
tuan-nhom1 <congtuan05102006@gmail.com>
2026-03-27 13:30:07 +0700

    bai3

--- LeCongTuan_24DH260219/baitap3.py
+++ LeCongTuan_24DH260219/baitap3.py
@@ -1,2 +1,9 @@
-KT="example@gmail.com"
-print(KT.endswith("@gmail.com"))
+email = input("Hãy điền email: ")
+
+def Kt(email):
+    if email.endswith("@gmail.com"):
+        print("email hợp lệ")
+    else:
+        print("email ko hợp lệ")
+
+Kt(email)
