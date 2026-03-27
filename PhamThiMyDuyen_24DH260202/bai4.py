bcae9620cf139210d7251f5aa41173d4f3ecd5fe
duyen_nhom8 <duyen5705@gmail.com>
2026-03-27 14:00:49 +0700

    baitap

---
+++ PhamThiMyDuyen_24DH260202/bai4.py
@@ -0,0 +1,23 @@
+def tich(a,b):
+    print("tich cua hai so la:", a*b)
+tich(4,6)
+
+def hieu(c,d):
+    print("hieu cua hai so la: ", (c-d))
+hieu(23,10)
+
+def tong(e,p):
+    print("tong hai so la:",(e+p))
+tong(8,11)
+
+def chia(g,h):
+    print("thuong cua hai so:", g/h)
+chia(16,2)
+
+def hienthi(n):
+    print("so vua nhap:",n)
+hienthi(200)
+
+def binhphuong(l):
+    print("binh phuong cua so la:",l*l)
+binhphuong(7)
